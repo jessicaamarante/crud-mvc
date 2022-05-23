6fef7f4fb4d0773798f68e200f782ad7a410d44d
Jessica do Amarante <jessica.amarante@duotecnologia.com.br>
2022-05-23 18:38:22 -0300

    Initial commit

---
+++ src/main/java/com/example/crudmvc/MainController.java
@@ -0,0 +1,13 @@
+package com.example.crudmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String showHomePage() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/example/crudmvc/CrudMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
